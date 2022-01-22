d5a44c36a470b08a7cd1a52c59b0155895a908c5
Jose Rodrigo <acg_1349@hotmail.com>
2022-01-21 22:44:16 -0500

    Se agregó dependencia y configuracion de Spring Security

--- src/main/java/com/z/vetdbz/VetDbZApplication.java
+++ src/main/java/com/z/vetdbz/VetDbZApplication.java
@@ -2,12 +2,19 @@ package com.z.vetdbz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class VetDbZApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(VetDbZApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(VetDbZApplication.class, args);
+    }
+
+    // @Bean
+    // public BCryptPasswordEncoder passwordEncoder() {
+    // return new BCryptPasswordEncoder();
+    // }
 
 }
